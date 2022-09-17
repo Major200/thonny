2028dd95df2d3174c449e366bc577c1e8b38bc42
Major200 <113383393+Major200@users.noreply.github.com>
2022-09-17 16:51:16 +0100

    Add files via upload

---
+++ idle adventure.py
@@ -0,0 +1,21 @@
+name=input('what is your name')
+print('Hello',name)
+x = int(input('whats the answer to 2 + 2: '))
+if x == 4:
+    print ('correct! Next question!')
+else: print ('thats not right!')    
+y = int(input('whats the answer to 5 + 6: '))
+if y == 11:
+    print ('correct! Next question!')
+else: print ('thats not right!')
+z = int(input('whats the answer to 10 - 7: '))
+if z == 3:
+    print ('the last one correct!')
+else: print ('your a dumb-ass')
+print ('if you got anything wrong, the answers are 4, 11 and three. Well done',name)
+ridle = int(input('Lets do some ridles, what flys but does not have wings, and crys but has no eyes? '))
+if ridle == cloud:
+    print ('correct!')
+
+   
+        
